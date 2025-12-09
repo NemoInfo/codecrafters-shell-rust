1e29726ad2ef03e932cd0c808a6ae58c39dd8333
Aaron <aaronpanaitescu@gmail.com>
2025-12-09 03:01:19 +0000

    Locate executable files
    Signed-off-by: Aaron <aaronpanaitescu@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,38 @@
-use std::io::{self, Write};
+use std::{
+  io::{self, Write},
+  os::unix::fs::PermissionsExt,
+  path::PathBuf,
+};
+
+fn search(paths: &Vec<PathBuf>, command: &str) -> Option<String> {
+  for path in paths {
+    if path.is_file() {
+      let name = path.file_name()?.to_str()?;
+      let is_exec = path.metadata().ok()?.permissions().mode() & 0o111 != 0;
+      if name == command && is_exec {
+        return Some(path.display().to_string());
+      }
+    } else if path.is_dir() {
+      let entries = std::fs::read_dir(path).ok()?;
+      for entry in entries {
+        let path = entry.ok()?.path();
+        if path.is_file() {
+          let name = path.file_name()?.to_str()?;
+          let is_exec = path.metadata().ok()?.permissions().mode() & 0o111 != 0;
+          if name == command && is_exec {
+            return Some(path.display().to_string());
+          }
+        }
+      }
+    }
+  }
+  None
+}
 
 fn main() {
+  let path = std::env::var("PATH").unwrap();
+  let paths: Vec<_> = std::env::split_paths(&path).collect();
+
   loop {
     print!("$ ");
     io::stdout().flush().unwrap();
@@ -23,10 +55,16 @@ fn main() {
             println!("{command} is a shell builtin");
             io::stdout().flush().unwrap();
           }
-          _ => {
-            println!("{command}: not found");
-            io::stdout().flush().unwrap();
-          }
+          _ => match search(&paths, command) {
+            Some(path) => {
+              println!("{command} is {path}");
+              io::stdout().flush().unwrap();
+            }
+            None => {
+              println!("{command}: not found");
+              io::stdout().flush().unwrap();
+            }
+          },
         }
       }
       invalid => {
