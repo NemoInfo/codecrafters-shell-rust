f31ffa9929a05a4f21f34cbcc37341336c5e1231
Aaron <aaronpanaitescu@gmail.com>
2025-12-09 02:07:15 +0000

    Command not found
    Signed-off-by: Aaron <aaronpanaitescu@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,7 @@ use std::io::{self, Write};
 fn main() {
   print!("$ ");
   io::stdout().flush().unwrap();
+  let mut command = String::new();
+  io::stdin().read_line(&mut command).expect("Expected command");
+  print!("{command}: command not found");
 }
