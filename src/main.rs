d26cd1539cdd329d1a42db1e9e42f22fb0e5f9ba
Aaron <aaronpanaitescu@gmail.com>
2025-12-09 02:18:38 +0000

    Exit command
    Signed-off-by: Aaron <aaronpanaitescu@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,12 @@ fn main() {
     io::stdout().flush().unwrap();
     let mut command = String::new();
     io::stdin().read_line(&mut command).expect("Expected command");
-    println!("{}: command not found", command.trim());
-    io::stdout().flush().unwrap();
+    match command.trim() {
+      "exit" => {break}
+      invalid => {
+        io::stdout().flush().unwrap();
+        println!("{}: command not found", invalid);
+      }
+    }
   }
 }
