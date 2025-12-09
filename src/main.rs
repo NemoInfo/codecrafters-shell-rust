852de33f26139fb2817145aac885f7686ce1894a
Aaron <aaronpanaitescu@gmail.com>
2025-12-09 02:29:00 +0000

    Echo command
    Signed-off-by: Aaron <aaronpanaitescu@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,18 @@ fn main() {
   loop {
     print!("$ ");
     io::stdout().flush().unwrap();
-    let mut command = String::new();
-    io::stdin().read_line(&mut command).expect("Expected command");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Expected command");
+    let mut args = input.trim().split(" ").collect::<Vec<_>>().into_iter();
+    let command = args.next().unwrap();
+
     match command.trim() {
-      "exit" => {break}
+      "exit" => break,
+      "echo" => {
+        let rest = args.collect::<Vec<_>>().join(" ");
+        println!("{rest}")
+      }
       invalid => {
         io::stdout().flush().unwrap();
         println!("{}: command not found", invalid);
